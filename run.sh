748218051f782e7bf00670607587f3e34f6eb658
lenia <164863959+leniamali@users.noreply.github.com>
2024-05-28 05:30:44 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-d1f0fffc-a2b2-41cd-b7ec-0bba15e81945/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-656bb10e-f42d-4aed-bbd1-5b09d52bc389/)
 
 
 echo "$curl_output" >> Update.tsx
